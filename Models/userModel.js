cccf148f65286d2a3fa1d7545d9cf7b0376f6a5d
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-22 22:17:17 +0600

    user model updated

--- Models/userModel.js
+++ Models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     },
     role : {
         type : String,
-        enum : ['farmer', 'buyer'],
+        enum : ['farmer', 'dealer'],
       
 },
     email : {
