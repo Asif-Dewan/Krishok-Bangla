ed6ea1dfd174e27920d30d8c8915997f8e4edcfe
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-24 20:05:34 +0600

    database connected

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 // set up view engine
 app.set('view engine','ejs')
@@ -16,6 +17,16 @@ app.use(middleware)
 app.get('/',(req,res) =>{
     res.render('pages/homepage',{title: 'Home page'})
 })
+const DB = `mongodb+srv://Krishok_Bangla:Krishok@bangla@cluster0.pdcxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(DB,({
+    useNewUrlParser : true,
+    useCreateIndex : true,
+    useUnifiedTopology : true,
+    useFindAndModify : false
+})).then(()=>{
+    console.log('Database connected successfully...')
+})
+
 
 const port = 3000;
 app.listen(port,()=>{
