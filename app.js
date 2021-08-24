8f41ba244c01fd6a4d39575400d530ba4f214dce
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-24 19:09:02 +0600

    homepage route

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const app = express()
+// set up view engine
+app.set('view engine','ejs')
+app.set('views','views')
+ 
+// middleware array
+const middleware = [
+    express.static('public'),
+    express.urlencoded({extended : true}),
+    express.json()
+]
+ 
+app.use(middleware)
 
 app.get('/',(req,res) =>{
-    res.status(200).json({
-        status: 'Success',
-        message: 'This route is for homepage'
-        
-        })
+    res.render('pages/homepage',{title: 'Home page'})
 })
 
 const port = 3000;
