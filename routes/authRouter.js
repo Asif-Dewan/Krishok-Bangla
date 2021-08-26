e082a439f728ebf0a8559e5d3464cc559541ebfc
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-26 19:51:19 +0600

    postRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController')
 const router = express.Router()
  
 router.get('/signup',authController.signupGetController)
+router.post('/signup',authController.signupPostController)
 
 module.exports = router
