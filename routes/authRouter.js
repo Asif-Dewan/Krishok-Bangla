1240b4f81106e63ada5ac19c15ff4b88a81d5e0d
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-26 20:59:03 +0600

    login post router added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,6 +8,7 @@ router.get('/signup',authController.signupGetController)
 router.post('/signup',authController.signupPostController)
 
 router.get('/login',authController.loginGetController)
+router.post('/login',authController.loginPostController)
  
 
 module.exports = router
