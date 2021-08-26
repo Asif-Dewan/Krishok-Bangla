3807fc501f3c8ca9f525c133536641a4ea17ddf8
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-26 20:57:39 +0600

    login post controller added

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,3 +28,23 @@ exports.signupPostController = async (req,res,next) =>{
 exports.loginGetController = (req,res,next) =>{
     res.render('pages/auth/login', {title : 'Login Your Account'})
 }
+exports.loginPostController = async (req,res,next) =>{
+    let {email, password} = req.body
+ 
+    try{
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(404).json({
+                status: 'Failed',
+                message : 'Invalid Credentials'
+            })
+        }
+        console.log('Successfully logged In',user)
+        console.log(`Your are logged in as a ${user.role}`)
+        res.render('pages/auth/login' , {title : 'Login Your Account'})
+ 
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
+}
