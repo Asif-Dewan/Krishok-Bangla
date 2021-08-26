3b295d37497a9b63f8a2887bd99e8e24eb66c6d8
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-26 19:33:34 +0600

    signup post controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,25 @@
+const User = require('../models/userModel')
 exports.signupGetController = (req,res,next) =>{
     res.render('pages/auth/signup', {title : 'Register Your Account'})
+}
+exports.signupPostController = async (req,res,next) =>{
+    
+    let {name,phone,address,nid,role,email,password} = req.body
+    try{
+        let user = new User({
+            name,
+            phone,
+            address,
+            nid,
+            role,
+            email,
+            password
+        })
+       let createdUser = await user.save() 
+       console.log('user created successfully',createdUser)
+       res.render('pages/auth/signup', {title : 'Register Your Account'})
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 }
