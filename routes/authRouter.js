433fcab2c149d3a996cdc74edfe720b8974fd922
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-27 12:03:30 +0600

    validator middleware for signup

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
- 
+const signupValidator = require('../validator/auth/signupValidator')
  
 const authController = require('../controllers/authController')
 const router = express.Router()
  
 router.get('/signup',authController.signupGetController)
-router.post('/signup',authController.signupPostController)
+router.post('/signup',signupValidator,authController.signupPostController)
 
 router.get('/login',authController.loginGetController)
 router.post('/login',authController.loginPostController)
