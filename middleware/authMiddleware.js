4341d9713d8a0b8dde039f304cb3350a1c899456
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-30 23:46:06 +0600

    Is authenticate function created

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,3 +19,11 @@ exports.bindUserWithRequest = () =>{
     }
 }
  
+exports.isAuthenticated = (req,res,next) =>{
+    if(!req.session.isLoggedIn){
+        return res.redirect('/auth/login')
+    }
+    next()
+}
+ 
+
