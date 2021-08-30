7988addc501b371fd39cd6eb923e3df676dc470e
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-30 17:08:01 +0600

    loginValidator added in routes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const signupValidator = require('../validator/auth/signupValidator')
+const loginValidator = require('../validator/auth/loginValidator')
+ 
  
 const authController = require('../controllers/authController')
 const router = express.Router()
@@ -8,7 +10,7 @@ router.get('/signup',authController.signupGetController)
 router.post('/signup',signupValidator,authController.signupPostController)
 
 router.get('/login',authController.loginGetController)
-router.post('/login',authController.loginPostController)
+router.post('/login',loginValidator,authController.loginPostController)
  
 
 module.exports = router
