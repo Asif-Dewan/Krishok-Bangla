6dd033647416047a3bf9cf023487e20c3fc4ec16
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-08-31 00:13:07 +0600

    Is unauthentcate middleware used in routes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const signupValidator = require('../validator/auth/signupValidator')
 const loginValidator = require('../validator/auth/loginValidator')
- 
+const authMiddleware = require('../middleware/authMiddleware')
  
 const authController = require('../controllers/authController')
 const router = express.Router()
  
-router.get('/signup',authController.signupGetController)
-router.post('/signup',signupValidator,authController.signupPostController)
-
-router.get('/login',authController.loginGetController)
-router.post('/login',loginValidator,authController.loginPostController)
+router.get('/signup',authMiddleware.isUnAuthenticate,authController.signupGetController)
+router.post('/signup',authMiddleware.isUnAuthenticate,signupValidator,authController.signupPostController)
+router.get('/login',authMiddleware.isUnAuthenticate,authController.loginGetController)
+router.post('/login',authMiddleware.isUnAuthenticate,loginValidator,authController.loginPostController)
 
 router.get('/logout',authController.logoutController)
  
