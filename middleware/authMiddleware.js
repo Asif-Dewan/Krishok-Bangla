951b82bb0bc4afa89af756cc0d88810b10120499
NazifaMuniat <nazifa.muniat@northsouth.edu>
2021-09-03 23:41:55 +0600

    middleware documentation added

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel')
  
+/**
+ * This middleware function will bind the user with request object.
+ * @module authMiddleware/bindUser
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ * 
+ */
 exports.bindUserWithRequest = () =>{
     return async (req,res,next) => {
         if(!req.session.isLoggedIn){
@@ -18,6 +27,15 @@ exports.bindUserWithRequest = () =>{
  
     }
 }
+/**
+ * This middleware function will check if the user is authenticated or not.
+ * @module authMiddleware/isAuthenticated
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ * 
+ */
  
 exports.isAuthenticated = (req,res,next) =>{
     if(!req.session.isLoggedIn){
@@ -25,7 +43,16 @@ exports.isAuthenticated = (req,res,next) =>{
     }
     next()
 }
- 
+ /**
+ * This middleware function will check if the user is Unauthenticated or not.
+ * @module authMiddleware/isUnAuthenticated
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ * 
+ */
+
 exports.isUnAuthenticated = (req,res,next) =>{
     if(req.session.isLoggedIn){
         return res.redirect('/dashboard')
