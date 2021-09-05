b0ec9bac54b44cf72b3e3e44fce91a0f7db4b6f2
Asif-Dewan <52652757+Asif-Dewan@users.noreply.github.com>
2021-09-05 20:58:47 +0600

    Delete Controllers directory

--- Controllers/controller.js
+++
@@ -1,9 +0,0 @@
-const Market = require('../models/OrderInfo')
-
-exports.OrderInfoGetController = (res) =>{
-    res.render('pages/auth/PlaceOrder', {
-        title : 'PlaceOrder' , 
-        error : {}, 
-        value : {}
-    })
-}
